688127781df6ce7292ccb9c6254fbc4657765bcd
Akash Saravanan <akashsara@outlook.com>
2017-09-24 00:30:17 +0530

    Reworked dynamic enemy spawn probabilities.

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -10,31 +10,19 @@ public class GameController : MonoBehaviour {
 	public GUIText scoreText;
 	public GUIText restartText;
 	public GUIText gameOverText;
-	private int hazardCount;
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
 
-	private int level;
-	private int[] levelUpArray = new int[] {500, 1000, 1500, 2500, 5000, 10000};
+	private int hazardCount;
+	private int difficultyLevel;
 	private float score;
 	private bool gameOver;
 	private bool restart;
 
-	private Dictionary<string, int[]> enemyGeneration = new Dictionary<string, int[]>(){
-		{"0", new int[] {0, 30, 60, 90, 100}},
-		{"1", new int[] {0, 25, 50, 75, 100}},
-		{"2", new int[] {0, 23, 46, 69, 99, 100}},
-		{"3", new int[] {0, 22, 44, 65, 95, 99, 100}},
-		{"4", new int[] {0, 15, 30, 45, 90, 95, 100}},
-		{"5", new int[] {0, 10, 20, 30, 80, 95, 100}},
-		{"6", new int[] {0, 10, 20, 25, 75, 90, 100}},
-		{"asteroidsField", new int[] {0, 30, 60, 90, 90, 100, 100}},
-		{"enemyBase", new int[] {0, 0, 0, 0, 95, 95, 100}}
-	};
-
 	int chooseSpawn() {
-		int [] minMaxValues = enemyGeneration[level.ToString()];
+		//25 - x, 25 - x, 25 - x, 35, 0 + x, 2*(-5 + x)
+		int [] minMaxValues = new int[] {0, 25 - difficultyLevel, 50 - (2 * difficultyLevel), 75 - (3 * difficultyLevel), (75 - (3 * difficultyLevel)) + 35, ((75 - (3 * difficultyLevel)) + 35) + difficultyLevel, ((75 - (3 * difficultyLevel)) + 35 + difficultyLevel ) + (2 * (-5 + difficultyLevel))};
 		int chosen = Random.Range(0, 100);
 		for(int i = 0; i < minMaxValues.Length - 1; i++) {
 			if(chosen >= minMaxValues[i] && chosen < minMaxValues[i + 1])
@@ -46,7 +34,6 @@ public class GameController : MonoBehaviour {
 
 	void Start() {
 		score = 0;
-		level = 0;
 		hazardCount = 10;
 		gameOver = false;
 		restart = false;
@@ -60,10 +47,10 @@ public class GameController : MonoBehaviour {
 		if(restart && Input.GetKeyDown(KeyCode.R)) {
 			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 		}
-		if(score > levelUpArray[level] && level != 6) {
-			level++;
-		}
 		hazardCount = 10 + Mathf.FloorToInt(score/250);
+		if(difficultyLevel < 15) {
+			difficultyLevel = Mathf.Max(difficultyLevel, Mathf.FloorToInt(score/50));//Random.Range(500, 1000)));
+		}
 	}
 
 	void FixedUpdate() {
